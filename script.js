3ab9b0aedbe7d35123f4f959aadc8b470a52a047
Matthew Kwong <mkwong.sh@mysummitps.org>
2017-01-24 06:29:19 +0000

    create personal website

---
+++ script.js
@@ -0,0 +1,7 @@
+          $(document).ready(function() {
+              $("#logo").effect("bounce", {
+                  times: 1
+              }, 530);
+          });
+           // background is as big as the screen size
+           // document.body.style.height = window.innerHeight;

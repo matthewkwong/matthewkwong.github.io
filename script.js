26d5d1c1757558754a6c8dcf83de588165594843
Matthew Kwong <10681480+MatthewKwong@users.noreply.github.com>
2019-07-17 21:33:26 -0700

    pushing out new updated version of site

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-          $(document).ready(function() {
-              $("#logo").effect("bounce", {
-                  times: 1
-              }, 530);
-          });
-           // background is as big as the screen size
-           // document.body.style.height = window.innerHeight;
+console.log("javascript is active!");
+
+$(document).ready(function() {
+  $("#logo").effect("bounce", {
+      times: 1
+  }, 530);
+});
+// background is as big as the screen size
+// document.body.style.height = window.innerHeight;
+
+
+$(document).ready(function() {
+  //scroll feature  - Learn More
+  $("#about-nav").click(function(){
+    $('html, body').animate({
+      scrollTop:$("#about").offset().top - 100
+    }, 1000);
+  });
+
+
+  $("#portfolio-nav").click(function(){
+    $('html, body').animate({
+      scrollTop:$("#portfolio").offset().top - 100
+    }, 1000);
+  });
+});

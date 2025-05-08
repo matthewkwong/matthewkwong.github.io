8f44de56d91c8364c13d10ab31b9abbb3b8fdb3a
Matthew Kwong <matthewrkwong@gmail.com>
2025-05-08 01:10:26 -0700

    update footer and autoplay on ios

--- script.js
+++ script.js
@@ -24,3 +24,61 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+$(document).ready(function(){
+    /*Fades in page on load */
+    $('body, .about-me-wrapper').css('display', 'none');
+    $('body, .about-me-wrapper').fadeIn(1000);
+  });
+  
+  // Image - Fullscreen on click
+  window.onload = () => {
+    let images = document.querySelectorAll("img");
+    let fullscreen = document.getElementById("fullscreen");
+   
+  // Clones into fullscreen div
+    if (images.length > 0) { for (let x of images) {
+      x.onclick = () => {
+        let clone = x.cloneNode();
+        clone.className = "";
+        fullscreen.innerHTML = "";
+        fullscreen.appendChild(clone);
+        fullscreen.className = "show";
+      };
+    }}
+  
+    fullscreen.onclick = () => {
+      fullscreen.className = "";
+    };
+  };
+  
+  
+  // Fullscreen Video
+  var elem = document.querySelector(".vid");
+  
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  
+  
+  // Disable Scroll on nav
+  function disable(){
+    document.querySelector('body').classList.add('disable-scroll');
+  }
+  
+  function enable(){
+    document.querySelector('body').classList.remove('disable-scroll');
+  }
+  
+  document.querySelector('#hamburger').addEventListener('click', disable);
+  document.querySelector('#close').addEventListener('click', enable);
+  
+  // Display year at bottom of nav
+  const year = new Date().getFullYear();
+  document.getElementById("year").innerHTML = year;
